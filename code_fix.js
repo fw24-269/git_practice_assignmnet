b749927a32692f3541c4771770436ebf5191c10d
KrunalGurao <krunalgurao1@gmail.com>
2022-12-01 15:09:56 +0530

    bug fixed

---
+++ code_fix.js
@@ -0,0 +1,14 @@
+//check for Prime Number
+
+let count=0;
+    for(i=1;i<=num;i++){
+        if(num%i==0){
+            count++;
+        }
+    }
+    if(count==2){
+    console.log("Yes");
+}
+else{
+    console.log("No");
+}
--- program_file.js
+++ program_file.js
@@ -1,7 +1,7 @@
 //check for Palindrome
 
  let bag="";
-    for(i=N-1;i>=0;i++){
+    for(i=N-1;i>=0;i--){
       bag=bag+str[i];
       
       
@@ -16,17 +16,3 @@
 
 
 
-//check for Prime Number
-
-let count=0;
-    for(i=1;i<=num;i++){
-        if(num%i==0){
-            count++;
-        }
-    }
-    if(count==1){
-    console.log("Yes");
-}
-else{
-    console.log("No");
-}
